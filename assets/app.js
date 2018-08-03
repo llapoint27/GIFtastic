bf86dff3f5b09cb6104dfc12895b5dfecbbf9033
Marco Polo <laurenlapoint@Laurens-MacBook-Pro.local>
2018-08-03 15:07:42 -0400

    update

---
+++ assets/app.js
@@ -0,0 +1,43 @@
+
+var categories = ["Michael Jackson", "Volleyball", "Coffee", "Queen"];
+
+function alertGifName() {
+  var gifName = $(this).attr("data-name");
+
+  alert(gifName);
+
+//add queryURL
+//addAJAX call
+//.then function
+
+}
+
+
+
+function renderButtons() {
+  $("#buttons-view").empty();
+
+  for (var i = 0; i < categories.length; i++) {
+
+
+    var a = $("<button>");
+    a.addClass("gif");
+    a.attr("data-name", categories[i]);
+    a.text(categories[i]);
+    $("#buttons-view").append(a);
+  }
+}
+
+$("#add-category").on("click", function(event) {
+
+  event.preventDefault();
+  var input = $("#category-input").val().trim();
+  categories.push(input);
+  renderButtons();
+
+});
+
+$(document).on("click", ".gif", alertGifName);
+
+renderButtons();
+
